1347476f698d41440e39729c58f2fec25d7c4e91
Fatwa <2402006@students.poltas.ac.id>
2026-07-02 12:23:50 +0700

    Menambahkan isi HTML, CSS, dan JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Branch fitur-deskripsi aktif");
+
+const deskripsi = document.getElementById("deskripsi");
+const btnUbah = document.getElementById("btnUbah");
+
+if (deskripsi && btnUbah) {
+    btnUbah.addEventListener("click", function () {
+        deskripsi.textContent = "Deskripsi berhasil diubah menggunakan JavaScript.";
+    });
+}
